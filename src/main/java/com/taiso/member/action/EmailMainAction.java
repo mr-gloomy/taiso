f5acfe4d2faf662988420026e59be7a15c15e949
김지은 <@DESKTOP-KHMATSK>
2022-11-28 20:55:31 +0900

    회원가입 메일

---
+++ src/main/java/com/taiso/member/action/EmailMainAction.java
@@ -0,0 +1,24 @@
+package com.taiso.member.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class EmailMainAction implements Action {
+
+	@Override
+	public ActionForward execute(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		System.out.println(" M : EmailMainAction_execute() 호출 ");
+		
+		HttpSession session = request.getSession();
+		session.removeAttribute("mem_id");
+		ActionForward forward = new ActionForward();
+		forward.setPath("./ReservationMain.rez");
+		forward.setRedirect(true);
+		
+		
+		return forward;
+	}
+
+}
